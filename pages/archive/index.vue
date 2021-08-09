d49084c32d4398a2f5de2e22996752800611715f
Philo van Kemenade <philovankemenade@gmail.com>
2021-08-09 13:04:03 +0200

    add type filter for Archive Cards

--- pages/archive/index.vue
+++ pages/archive/index.vue
@@ -1,20 +1,31 @@
 <template>
-  <CardPage
-    :cards="cards"
-    :title="title"
-    :card-path="cardPath"
-    :data-class="dataClass"
-  />
+  <Fragment>
+    <Tags
+      :tags="types"
+      :filter="typesFilter"
+      :highlight="[]"
+      @toggle-tag="toggleType"
+      @set-tag="setType"
+    />
+    <CardPage
+      :cards="filteredCards"
+      :title="title"
+      :card-path="cardPath"
+      :data-class="dataClass"
+    />
+  </Fragment>
 </template>
 
 <script>
+import { Fragment } from 'vue-fragment'
 import CardPage from '~/components/CardPage'
+import Tags from '~/components/visual/Tags'
 import { getLocalePath } from '~/util/contentFallback'
 
 const dataClass = 'archive'
 
 export default {
-  components: { CardPage },
+  components: { Fragment, Tags, CardPage },
   async asyncData({ $content, app }) {
     const path = dataClass
 
@@ -33,11 +44,42 @@ export default {
     title: dataClass,
     cardPath: dataClass + '-slug',
     dataClass,
+    types: ['project', 'example'],
+    typesFilter: [],
   }),
   head() {
     return {
       title: this.$t(dataClass),
     }
+  },
+  computed: {
+    filteredCards() {
+      return this.typesFilter.length === 0
+        ? this.cards
+        : this.cards.filter(
+            (card) => card.type && this.typesFilter.includes(card.type)
+          )
+    },
+  },
+  methods: {
+    toggleType(type) {
+      if (this.typesFilter.includes(type)) {
+        this.typesFilter = this.typesFilter.filter((t) => t !== type)
+      } else {
+        this.typesFilter = [...this.typesFilter, type]
+      }
+
+      if (this.typesFilter.length === this.types.length) {
+        this.typesFilter = []
+      }
+    },
+    setType(type) {
+      if (this.typesFilter.includes(type)) {
+        this.typesFilter = []
+      } else {
+        this.typesFilter = [type]
+      }
+    },
   },
 }
 </script>
