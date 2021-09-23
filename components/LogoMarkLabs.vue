1e972608ba3af6f196557bc3d9e4c2458f65f6f6
Philo van Kemenade <philovankemenade@gmail.com>
2021-09-23 15:29:52 +0200

    change logotype order
    to be in sync with descriptions on about page

--- components/LogoMarkLabs.vue
+++ components/LogoMarkLabs.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="text-subtitle-1 font-weight-bold text-uppercase white--text">
-    <span class="v-mid">LABS</span>
+    <span class="v-mid">SOUND AND VISION</span>
     <v-icon class="v-mid subtitle-2" :style="{ color: '#009fda' }"
       >mdi-circle</v-icon
     >
-    <span class="v-mid">SOUND AND VISION</span>
+    <span class="v-mid">LABS</span>
   </div>
 </template>
 
