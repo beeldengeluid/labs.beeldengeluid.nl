b2cae724d696c1cbd724df1eba4186798e28cbfc
Philo van Kemenade <philovankemenade@gmail.com>
2021-11-04 11:35:15 +0100

    make Card styles scoped

--- components/Card.vue
+++ components/Card.vue
@@ -113,7 +113,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .v-card {
   box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.05),
     0px 1px 1px 0px rgba(0, 0, 0, 0.035), 0px 1px 3px 0px rgba(0, 0, 0, 0.03) !important;
