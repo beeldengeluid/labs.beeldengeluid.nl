8d6c5d923d2ebec10f2178bb0fb732af31912768
Philo van Kemenade <philovankemenade@gmail.com>
2021-12-24 17:02:54 +0100

    use axios instead of node-fetch
    to prevent issues with CommonJS require() statements

--- scripts/fetch-datacatalog.mjs
+++ scripts/fetch-datacatalog.mjs
@@ -1,14 +1,17 @@
-import fetch from 'node-fetch'
+import axios from 'axios'
 import fs from 'fs'
 
 const datacatalogURL = 'https://data.beeldengeluid.nl/id/datacatalog/0001'
 const datacatalogPath = 'content/datacatalog0001.json'
 
-fetch(datacatalogURL)
-  .then((res) => res.json())
-  .then((json) => {
-    fs.writeFile(datacatalogPath, JSON.stringify(json), (err) => {
+axios
+  .get(datacatalogURL)
+  .then(function (response) {
+    fs.writeFile(datacatalogPath, JSON.stringify(response.data), (err) => {
       if (err) throw err
       console.log(`Data written to file: ${datacatalogPath}`)
     })
+  })
+  .catch(function (error) {
+    console.log(error)
   })
