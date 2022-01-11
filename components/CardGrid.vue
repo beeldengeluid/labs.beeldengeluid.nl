afb4e802e2072f511b2421f2d6faf50ffe8e60fe
Philo van Kemenade <philovankemenade@gmail.com>
2022-01-11 11:48:09 +0100

    rename Card to CardItem

--- components/CardGrid.vue
+++ components/CardGrid.vue
@@ -6,16 +6,16 @@
       :cols="card.flex"
       class="mt-2 flex-md-grow-0"
     >
-      <Card :card="card" :path="path" :data-class="dataClass" />
+      <CardItem :card="card" :path="path" :data-class="dataClass" />
     </v-col>
   </v-row>
 </template>
 
 <script>
-import Card from '~/components/Card'
+import CardItem from '~/components/CardItem'
 
 export default {
-  components: { Card },
+  components: { CardItem },
   props: {
     cards: { type: Array, required: true, default: () => [] },
     path: { type: String, required: true, default: '' },
