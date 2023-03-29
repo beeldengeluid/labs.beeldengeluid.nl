b56339edf24f68b6d934e866707f8f116bd8c319
Philo van Kemenade <philovankemenade@gmail.com>
2023-03-29 15:08:05 +0200

    copy config files & update vuetify path
    from 'vuetify/es5/util/colors' to "vuetify/lib/util/colors"

---
+++ config/menu.js
@@ -0,0 +1,57 @@
+import icons from './icons'
+import { classColors } from './theme'
+
+export default [
+  {
+    icon: icons.home,
+    title: 'home',
+    to: 'index',
+  },
+  {
+    icon: icons.lab,
+    title: 'labs',
+    to: 'labs',
+    color: classColors.lab,
+  },
+  {
+    icon: icons.project,
+    title: 'projects',
+    to: 'projects',
+    color: classColors.project,
+  },
+  {
+    icon: icons.topic,
+    title: 'topics',
+    to: 'topics',
+    color: classColors.topic,
+  },
+  {
+    icon: icons.dataset,
+    title: 'datasets',
+    to: 'datasets',
+    color: classColors.dataset,
+  },
+  {
+    icon: icons.api,
+    title: 'apis',
+    to: 'apis',
+    color: classColors.api,
+  },
+  {
+    icon: icons.blog,
+    title: 'blogs',
+    to: 'blogs',
+    color: classColors.blog,
+  },
+  {
+    icon: icons.archive,
+    title: 'archive',
+    to: 'archive',
+    color: classColors.archive,
+  },
+  {
+    icon: icons.about,
+    title: 'about',
+    to: 'about',
+  },
+]
