5d51d35600e087b4fe933e3843c4d2826a94e16d
Govert Brinkmann <gbrinkmann@beeldengeluid.nl>
2023-07-25 17:36:52 +0000

    Migrate theme to vuetify config

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,3 +1,4 @@
+import theme from '../config/theme'
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
@@ -7,6 +8,24 @@ export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     // @ts-ignore
     ssr: appConfig.ssr,
+    theme: {
+      defaultTheme: 'light',
+      themes: {
+        light: {
+          dark: false,
+          colors: theme,
+        },
+        dark: {
+          dark: false,
+          colors: theme,
+        },
+      },
+      variations: {
+        colors: ['nisvlightblue', 'nisvdarkblue'],
+        lighten: 6,
+        darken: 6,
+      },
+    },
   })
   nuxtApp.vueApp.use(vuetify)
 })
